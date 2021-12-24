cac46488d2755563fab89482c559a8c3abffd55e
SaurabhAkolkar <37813446+SaurabhAkolkar@users.noreply.github.com>
2021-12-24 19:13:13 +0530

    ApiResponse interface added

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,88 +1,47 @@
+const { success, failure } = require("../interface/ApiResponse");
 const Product = require("../model/Product");
 
 exports.create_product = async (req,res)=>{
   try {
-      const data = await Product.create(req.body);
-      res.status(201).json({
-          status:true,
-          message:"Product created",
-          product:data
-      })
-      
+      const product = await Product.create(req.body);
+      res.status(201).json(success(true,"Product created",product));      
   } catch (error) {
-      res.status(401).json({
-          status:false,
-          message:`Something went wrong! Please try again ${error}`,
-          product:null
-      })
-      
+      res.status(401).json(failure(false,`Something went wrong! Please try again ${error}`,[]));    
   }
 };
 
 exports.show_products = async (req,res)=>{
     try {
         const products = await Product.find();
-        res.status(200).json({
-            status:true,
-            message:"Retrive successfully",
-            products:products
-        })
+        res.status(200).json(success(true,"Retrive successfully",products));
     } catch (error) {
-        res.status(401).json({
-            status:false,
-            message:`Data not found ${error}`,
-            product:null
-        })
+        res.status(401).json(failure(false,`Something went wrong! Please try again ${error}`,[]));
     }
 };
 
 exports.update_product = async(req,res)=>{
     try {
         await Product.updateOne({_id:req.params.id},{$set:req.body});
-        res.status(200).json({
-            status:true,
-            message:"Data update successfully"
-        })
-       
+        res.status(200).json(success(true,"Data update successfully",[]));       
     } catch (error) {
-        res.status(401).json({
-            status:false,
-            message:`not updated ${error}`
-        })
+        res.status(401).json(failure(false,`Something went wrong! Please try again ${error}`,[]));
     }
 };
 
 exports.delete_product = async (req,res)=>{
     try {
         await Product.findByIdAndDelete(req.params.id);
-        res.status(200).json({
-            status:true,
-            message:"Product deleted successfully"
-        })
-        
+        res.status(200).json(success(true,"Deleted successfully",[]));
     } catch (error) {
-        res.status(401).json({
-            status:false,
-            message:`not deleted ${error}`
-        })
-        
+        res.status(401).json(failure(false,`Something went wrong! Please try again ${error}`,[]));     
     }
 };
 
 exports.show_product = async (req,res)=>{
     try {
         const product = await Product.findById(req.params.id);
-        res.status(200).json({
-            status:true,
-            message:"Retrive successfully",
-            product:product
-        })
+        res.status(200).json(success(true,"Retrive successfully",product));
     } catch (error) {
-        res.status(401).json({
-            status:false,
-            message:`Not found ${error}`,
-            product:null
-        })
-        
+        res.status(401).json(failure(false,`Something went wrong! Please try again ${error}`,[]));
     }
 };
